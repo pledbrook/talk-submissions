e0efc0707258e55ecf6fe63b64cdd4760264bcef
Peter Ledbrook <peter@cacoethes.co.uk>
2013-06-24 15:57:35 +0100

    Switch to Heroku and update SS OAuth plugin.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,21 +15,28 @@ grails.project.dependency.resolution = {
         grailsCentral()
         mavenCentral()
         mavenRepo "http://maven.springframework.org/milestone/"
-        mavenRepo "http://guice-maven.googlecode.com/svn/trunk/"
+        mavenLocal()
     }
     dependencies {
-        runtime "mysql:mysql-connector-java:5.1.17"
+        runtime "mysql:mysql-connector-java:5.1.17", "com.google.inject:guice:3.0"
+
     }
 
     plugins {
+        compile ":heroku:1.0.1", ":cloud-support:1.0.8", {
+            exclude "database-session"
+        }
+
         compile ":hibernate:$grailsVersion",
                 ":spring-security-core:1.2.7.3",
-                ":spring-security-oauth:2.0.1.1",
-                ":spring-security-openid:1.0.4"
+                ":spring-security-oauth:2.0.2",
+                ":spring-security-openid:1.0.4", {
+            excludes "guice", "spock", "spock-grails-support"
+        }
 
         runtime ":resources:1.2-RC1",
                 ":jquery:1.8.0",
-                ":cloud-foundry:1.2.3",
+//                ":cloud-foundry:1.2.3",
                 ":database-migration:1.1",
                 ":markdown:1.0.0.RC1",
                 ":modernizr:2.6.2",
